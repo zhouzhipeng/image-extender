424c43890a519b77374560f19800416f42bae904
ronnie <ronnieinus@gmail.com>
2026-06-11 17:10:30 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
